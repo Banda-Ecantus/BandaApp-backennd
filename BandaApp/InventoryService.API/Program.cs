8488e5fc4d49358f09f9773d15aa29ac17037631
Vinicius Soares Silva de Souza <vinicius.soares@castgroup.com.br>
2025-02-08 19:17:53 -0300

    adicionando tratamento de exceção global aos modulos

--- BandaApp/InventoryService.API/Program.cs
+++ BandaApp/InventoryService.API/Program.cs
@@ -1,3 +1,5 @@
+using Shared.ExceptionHandling;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Adicionando o Middleware de Tratamento de Exceções
+app.UseGlobalExceptionHandling();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
