8423f2ddad72872fc2bcf99f37c0531fbd40924c
Vinicius Soares Silva de Souza <vinicius.soares@castgroup.com.br>
2025-03-07 11:40:17 -0300

    validação dto

--- BandaApp/InventoryService.Application/DTOS/InventoryItemDto.cs
+++ BandaApp/InventoryService.Application/DTOS/InventoryItemDto.cs
@@ -1,16 +1,22 @@
 ﻿using InventoryService.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace InventoryService.Application.DTOS
 {
     public class InventoryItemDto
     {
         public Guid? Guid { get; set; }
-        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public required string Name { get; set; }
 
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = "Número de Série é obrigatório.")]
         public required string SerialNumber { get; set; }
 
+        [Required(ErrorMessage = "Categoria é obrigatória.")]
         public required Guid CategoryId { get; set; }
 
         public string? Type { get; set; }
